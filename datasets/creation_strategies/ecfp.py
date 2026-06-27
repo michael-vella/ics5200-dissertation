21e851c212e3dce80a87f708cad9c626b47645ce
Michael Vella <michael.vella0110@gmail.com>
2026-06-27 12:30:04 +0200

    refactor: ecfp to not use deprecated methods

--- datasets/creation_strategies/ecfp.py
+++ datasets/creation_strategies/ecfp.py
@@ -2,7 +2,8 @@ from typing import override
 
 import pandas as pd
 import numpy as np
-import deepchem as dc
+from rdkit import Chem
+from rdkit.Chem import rdFingerprintGenerator
 
 from datasets.creation_strategies.base_strategy import DatasetCreationStrategy
 
@@ -26,7 +27,7 @@ class ECFPDatasetCreator(DatasetCreationStrategy):
         """
         Creates an ECFP-based dataset from a DataFrame containing SMILES strings.
 
-        Each SMILES string is featurised using a CircularFingerprint (ECFP4) with a
+        Each SMILES string is featurised using a MorganGenerator (ECFP4) with a
         radius of 2 and a fingerprint size of 2048 bits. The resulting fingerprint
         vector is stored as a new column in the DataFrame.
 
@@ -39,11 +40,13 @@ class ECFPDatasetCreator(DatasetCreationStrategy):
             pd.DataFrame: The input DataFrame with an additional 'mol' column
                         containing the ECFP fingerprint as a NumPy float32 array.
         """
-        self._logger.info("Initialising ECFP featuriser for feature extraction")
-        featurizer = dc.feat.CircularFingerprint(size=2048, radius=2)
+        self._logger.info("Initialising Morgan fingerprint generator for feature extraction")
+        generator = rdFingerprintGenerator.GetMorganGenerator(radius=2, fpSize=2048)
 
-        self._logger.info("Featurising SMILES strings into ECFP fingerprints")
-        df['mol'] = df['smiles'].apply(lambda x: np.float32(featurizer(x)[0])) # todo: getting deprecation warning to be replaced with MorganGenerator
+        self._logger.info("Featurising SMILES strings into ECFP fingerprints. Please wait this process might take a while.")
+        df['mol'] = df['smiles'].apply(
+            lambda x: np.float32(generator.GetFingerprintAsNumPy(Chem.MolFromSmiles(x)))
+        )
 
         self._logger.info(f"Featurisation complete. '{len(df)}' molecules processed")
         return df
